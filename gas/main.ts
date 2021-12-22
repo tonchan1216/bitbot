8fa32a075dbfeb68fbf70550cab5f30106ab25fb
tonchan <cleartone1216@gmail.com>
2021-12-22 10:05:44 +0900

    prettier

--- gas/main.ts
+++ gas/main.ts
@@ -1,6 +1,6 @@
 const prop = PropertiesService.getScriptProperties().getProperties()
-const coincheck_baseuri = "https://coincheck.com/api"
-const slack_baseuri = "https://slack.com/api/chat.postMessage"
+const coincheck_baseuri = 'https://coincheck.com/api'
+const slack_baseuri = 'https://slack.com/api/chat.postMessage'
 const COINCHECK_ACCESS_KEY = prop.COINCHECK_ACCESS_KEY
 const COINCHECK_SECRET_KEY = prop.COINCHECK_SECRET_KEY
 const SLACK_ACCESS_TOKEN = prop.SLACK_ACCESS_TOKEN
@@ -41,7 +41,7 @@ function postMessage(contents: Contents, thread_ts = '') {
     thread_ts: thread_ts,
     text: '',
     username: 'BitBot',
-    icon_emoji: ":robot_face:",
+    icon_emoji: ':robot_face:',
     blocks: JSON.stringify(contents),
   }
 
@@ -70,7 +70,7 @@ function getAccount() {
   const url = coincheck_baseuri + '/accounts/balance'
   const params: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
     method: 'get',
-    headers: hmac(url)
+    headers: hmac(url),
   }
 
   const response = UrlFetchApp.fetch(url, params)
@@ -78,18 +78,20 @@ function getAccount() {
 }
 
 // 取引実行
-function postOrder(rate:number, ammount:number, order_type = "buy", pair = "btc_jpy") {
+function postOrder(rate: number, ammount: number, orderType = 'buy', pair = 'btc_jpy') {
   const url = coincheck_baseuri + '/exchange/orders'
   const body = {
-    rate: rate,
-    ammount: ammount,
-    order_type: order_type,
-    pair: pair
+    pair: pair,
+    order_type: orderType,
+    rate: ['buy', 'sell'].includes(orderType) ? rate : null,
+    ammount: orderType == 'market_buy' ? null : ammount,
+    market_buy_amount: orderType == 'market_buy' ? ammount : null,
+    stop_loss_rate: null,
   }
   const params: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
     method: 'post',
     payload: body,
-    headers: hmac(url, body)
+    headers: hmac(url, body),
   }
 
   const response = UrlFetchApp.fetch(url, params)
@@ -98,26 +100,24 @@ function postOrder(rate:number, ammount:number, order_type = "buy", pair = "btc_
 }
 
 // HMAC認証ダイジェストの発行
-function hmac(url: string, data?: any) : GoogleAppsScript.URL_Fetch.HttpHeaders {
+function hmac(url: string, data?: any): GoogleAppsScript.URL_Fetch.HttpHeaders {
   const date = new Date()
-  var nonce = Math.floor(date.getTime()/1000).toString();
-  const message = (data) ? nonce + url + JSON.stringify(data) : nonce + url
+  var nonce = Math.floor(date.getTime() / 1000).toString()
+  const message = data ? nonce + url + JSON.stringify(data) : nonce + url
   const signature = Utilities.computeHmacSha256Signature(message, COINCHECK_SECRET_KEY)
-  const sign = signature.reduce(
-    (prev: string, current: number) => {
-      const hexstr = (current < 0 ? current + 256 : current).toString(16);
-      return prev + (hexstr.length == 1 ? '0' : '') + hexstr;
-    },''
-  );
+  const sign = signature.reduce((prev: string, current: number) => {
+    const hexstr = (current < 0 ? current + 256 : current).toString(16)
+    return prev + (hexstr.length == 1 ? '0' : '') + hexstr
+  }, '')
   return {
-    "ACCESS-KEY": COINCHECK_ACCESS_KEY,
-    "ACCESS-NONCE": nonce,
-    "ACCESS-SIGNATURE": sign
+    'ACCESS-KEY': COINCHECK_ACCESS_KEY,
+    'ACCESS-NONCE': nonce,
+    'ACCESS-SIGNATURE': sign,
   }
 }
 
 // エラー処理
-function errorHandle(header: string, msg:string) {
+function errorHandle(header: string, msg: string) {
   const contents: Contents = [
     {
       type: 'header',
@@ -144,7 +144,7 @@ function errorHandle(header: string, msg:string) {
 }
 
 // 取引実行のお知らせ
-function notification(msg : string) {
+function notification(msg: string) {
   const contents: Contents = [
     {
       type: 'header',
@@ -177,7 +177,7 @@ function sheetLogger(rate: number, evaluate: number, action: string, volume: num
   const row: number = (sheet?.getLastRow() ?? 1) + 1
   const now: Date = new Date()
 
-  sheet?.getRange(row, 1).setValue(Utilities.formatDate(now, "JST", "yyyy/MM/dd HH:mm:ss"))
+  sheet?.getRange(row, 1).setValue(Utilities.formatDate(now, 'JST', 'yyyy/MM/dd HH:mm:ss'))
   sheet?.getRange(row, 2).setNumberFormat('@')
   sheet?.getRange(row, 2).setValue(rate)
   sheet?.getRange(row, 3).setNumberFormat('@')
@@ -192,62 +192,62 @@ function getTarget(): number {
   const start: string = sheet?.getRange(1, 2).getValue()
   const volume: string = sheet?.getRange(2, 2).getValue()
 
-  const startDate = Date.parse(start);
+  const startDate = Date.parse(start)
   const today = new Date().getTime()
   const diff: number = Math.ceil((today - startDate) / 86400000)
 
-  return (diff > 0) ? diff * Number(volume) : 0
+  return diff > 0 ? diff * Number(volume) : 0
 }
 
 function main() {
   const account = getAccount()
-  if (account["success"] != true) {
-    errorHandle("missed getAccount", account["error"])
+  if (account['success'] != true) {
+    errorHandle('missed getAccount', account['error'])
     return
   }
 
   const ticker = getTicker()
-  const rate = Number(ticker["last"])
+  const rate = Number(ticker['last'])
 
-  const evaluate = account["jpy"] + account["jpy_reserved"] + rate * (account["btc"] + account["btc_reserved"])
+  const evaluate = account['jpy'] + account['jpy_reserved'] + rate * (account['btc'] + account['btc_reserved'])
 
   const targetVolume = getTarget()
 
   if (targetVolume == 0) {
-    errorHandle("missed targetVolume", "積み立て目標金額が設定できませんでした")
+    errorHandle('missed targetVolume', '積み立て目標金額が設定できませんでした')
     return
   }
 
-  let trade_type = "SKIP" // default
+  let action = 'SKIP' // default
   let tradeVolume = 0
   if (evaluate > targetVolume * 1.12) {
-    tradeVolume = evaluate - targetVolume
-    const res = postOrder(rate, tradeVolume, "sell")
+    tradeVolume = (evaluate - targetVolume) / rate // BTC
+    const res = postOrder(rate, tradeVolume, 'sell')
 
-    if (res["success"] != true) {
-      trade_type = "FAIL"
-      errorHandle("missed sell BTC", res["error"])
+    if (res['success'] != true) {
+      action = 'FAIL'
+      errorHandle('missed sell BTC', res['error'])
     } else {
-      trade_type = "SELL"
-      notification("SELL")
+      action = 'SELL'
+      notification('SELL')
     }
-  } else if(evaluate < targetVolume) {
-    if (account["jpy"] <= targetVolume-evaluate) {
-      trade_type = "SHORT"
-      errorHandle("missed buy BTC", "残高が不足しています")
+  } else if (evaluate < targetVolume) {
+    if (account['jpy'] <= targetVolume - evaluate) {
+      action = 'SHORT'
+      errorHandle('missed buy BTC', '残高が不足しています')
     } else {
-      tradeVolume = targetVolume - evaluate
-      const res = postOrder(rate, tradeVolume, "buy")
+      tradeVolume = targetVolume - evaluate // JPY
+      const res = postOrder(rate, tradeVolume, 'market_buy_amount')
 
-      if (res["success"] != true) {
-        trade_type = "FAIL"
-        errorHandle("missed buy BTC", res["error"])
+      if (res['success'] != true) {
+        action = 'FAIL'
+        errorHandle('missed buy BTC', res['error'])
       } else {
-        trade_type = "BUY"
-        notification("BUY")
+        action = 'BUY'
+        notification('BUY')
       }
     }
   }
 
-  sheetLogger(evaluate, rate, trade_type, tradeVolume)
+  sheetLogger(evaluate, rate, action, tradeVolume)
 }
